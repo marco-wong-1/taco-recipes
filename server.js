f12ca268bf3e677a14036c4a0a22520eb0a4a5fe
Marco Wong <marco@Marcos-Air.frontierlocal.net>
2020-03-28 21:52:25 -0400

    heroku fixes

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.get('/', (req, res) => res.send('API WORKING'));
 
 // Define Routes
