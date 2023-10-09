6be1c82c4ccf2b382786c200f697847023568307
Marco Wong <cpwmarco@gmail.com>
2023-10-09 18:05:51 -0400

    use path resolve

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/recipe', require('./routes/api/recipe'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
