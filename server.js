67d48b8ad98821c9877f1766e96afaf2a7b8b01b
Marco Wong <cpwmarco@gmail.com>
2023-10-09 16:51:08 -0400

    serverjs

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use('/api/ingredient', require('./routes/api/ingredient'));
 app.use('/api/recipe', require('./routes/api/recipe'));
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
